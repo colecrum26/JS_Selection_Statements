16dde989504114a3b896df0d54ce737e3c44960c
colecrum26 <cwcrumrine@gmail.com>
2023-03-13 11:17:17 -0500

    exercise one

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+
+let answer = window.prompt("guess a number:");
+  let favNum = 23;
+  if (answer < favNum) {
+    window.alert ("too low")
+  } else if (answer > favNum) {
+    window.alert ("too high")
+  } else {
+    window.alert ("congratulations!!")
+  }
+
+let birthMonth = window.prompt("What is your birth month?");
+  switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
