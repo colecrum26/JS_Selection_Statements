5aa526d7013c3631d4f5c8cbec8e34a81685e1f7
colecrum26 <cwcrumrine@gmail.com>
2023-03-13 11:26:22 -0500

    added console log

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+// Exercise 1
 let answer = window.prompt("guess a number:");
   let favNum = 23;
   if (answer < favNum) {
     window.alert ("too low")
+    console.log("too low")
   } else if (answer > favNum) {
     window.alert ("too high")
+    console.log("too high")
+  } else if (answer == number){
+    window.alert ("Congratulations!!")
+    console.log("Congratulations!!")
   } else {
-    window.alert ("congratulations!!")
+    window.alert("make sure to enter a number")
+    console.log("make sure to enter a number")
   }
 
+  // Exercise 2
 let birthMonth = window.prompt("What is your birth month?");
-  switch (key) {
+  switch (birthMonth) {
     case value:
       
       break;
