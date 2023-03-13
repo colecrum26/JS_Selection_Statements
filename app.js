0ea719bffccbcfe2bbd26c673b1d1e565018f826
colecrum26 <cwcrumrine@gmail.com>
2023-03-13 13:44:04 -0500

    refactored

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-// console.log("Hello World!\n==========\n");
-// console.log(
-//   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-// );
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
 
-// // Exercise 1
-// let answer = window.prompt("guess a number:");
-//   let favNum = 23;
-//   if (answer < favNum) {
-//     window.alert ("too low")
-//     console.log("too low")
-//   } else if (answer > favNum) {
-//     window.alert ("too high")
-//     console.log("too high")
-//   } else if (answer == favNum){
-//     window.alert ("Congratulations!!")
-//     console.log("Congratulations!!")
-//   } else {
-//     window.alert("make sure to enter a number!")
-//     console.log("make sure to enter a number!")
-//   }
+// Exercise 1
+let answer = window.prompt("guess a number:");
+let favNum = 23;
+if (answer < favNum) {
+  window.alert("too low");
+  console.log("too low");
+} else if (answer > favNum) {
+  window.alert("too high");
+  console.log("too high");
+} else if (answer == favNum) {
+  window.alert("Congratulations!!");
+  console.log("Congratulations!!");
+} else {
+  window.alert("make sure to enter a number!");
+  console.log("make sure to enter a number!");
+}
 
-//   // Exercise 2
-// let birthMonth = window.prompt("What is your birth month?");
-//   switch (birthMonth) {
-//     case "January":
-//       window.alert("You were born in winter.")
-//       break;
-//     case "March":
-//       window.alert("You were born in spring.")
-//       break;
-//     case "June":
-//       window.alert("You were born in summer.")
-//       break;
-//     case "October":
-//       window.alert("You were born in fall.")
-//     default: window.alert("Season not determined.")
-//       break;
-//   }
+// Exercise 2
+let birthMonth = window.prompt(
+  "What is your birth month? 1-Jan, 2-Feb, 3-March, 4-April, 5-May, 6-June, 7-July, 8-Aug, 9-Sept, 10-Oct, 11-Nov, 12-Dec"
+);
+switch (parseInt(birthMonth)) {
+  case 12:
+  case 1:
+  case 2:
+    window.alert("You were born in winter.");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    window.alert("You were born in spring.");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    window.alert("You were born in summer.");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    window.alert("You were born in fall.");
+  default:
+    window.alert("Please enter a number given.");
+    break;
+}
 
-  // Exercise 3
+// Exercise 3
 
 let typeId = "01";
 let colorId = "PU";
@@ -49,43 +60,42 @@ let color = "";
 let size = "";
 
 switch (typeId) {
-case "01": 
-  type = "Tank top";
- case "02": 
-  type = "T-Shirt";
- case "03": 
-  type = "Long Sleeve";
- case "04": 
-  type == "Sweat Shirt";
- default: 
-  type = "Other";
+  case "01":
+    type = "Tank top";
+  case "02":
+    type = "T-Shirt";
+  case "03":
+    type = "Long Sleeve";
+  case "04":
+    type == "Sweat Shirt";
+  default:
+    type = "Other";
 }
 
 switch (colorId) {
   case "BK":
-  color = "Black";
+    color = "Black";
   case "BL":
-  color = "Blue";
+    color = "Blue";
   case "RD":
-  color = "Red";
+    color = "Red";
   case "PU":
-  color = "Purple";
-  default: 
-  color = "White";
+    color = "Purple";
+  default:
+    color = "White";
 }
-
 
 switch (sizeId) {
   case "S":
-  size = "Small";
- case "M":
-  size = "Medium";
-case "L": 
-  size = "Large";
- case "XL": 
-  size = "Extra Large";
- default:
-  size = "One Size Fits All";
+    size = "Small";
+  case "M":
+    size = "Medium";
+  case "L":
+    size = "Large";
+  case "XL":
+    size = "Extra Large";
+  default:
+    size = "One Size Fits All";
 }
 
 console.log(`Product: ${size} ${color} ${type}`);
